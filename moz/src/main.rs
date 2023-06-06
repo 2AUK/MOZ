a7fc3f03d6b409892b6b8b08db594c7c6989ece4
2auk <ahmada1uk@googlemail.com>
2023-06-06 01:34:27 +0100

    setting up project structure

---
+++ moz/src/main.rs
@@ -0,0 +1,12 @@
+use std::f64::consts::PI;
+
+// Grid structs might be superfluous
+pub struct RadialGrid { }
+
+pub struct AngularGrid { }
+
+pub struct Grid { }
+
+fn main() {
+    println!("Hello, world!");
+}
