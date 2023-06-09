ef9d787d7e924194c09959ab179bcc49d10c6527
2auk <ahmada1uk@googlemail.com>
2023-06-09 12:26:01 +0100

    added test function for chebyshev-gauss

--- grids/src/chebyshev_gauss.rs
+++ grids/src/chebyshev_gauss.rs
@@ -14,12 +14,25 @@ impl ChebyshevGauss {
         let mut nodes = Vec::new();
         let mut weights = Vec::new();
         for i in 0..order {
-            nodes.push(0.5 * (b + a) + 0.5 * (b - a) * ( (2.0 * i as f64 + 1.0) / (2.0 * order as f64) * PI).cos())
+            nodes.push(0.5 * (b + a) + 0.5 * (b - a) * ( (2.0 * (order - i) as f64 - 1.0) / (2.0 * order as f64) * PI).cos())
         }
         for i in 0..order {
             weights.push(PI / order as f64);
         }
         ChebyshevGauss { a, b, order, nodes, weights }
+    }
+
+    // evaluates to 3.977463260506145
+    pub fn compute_exp(&self, x: f64) -> f64 {
+        x.exp()
+    }
+
+    pub fn integrate(&self) -> f64 {
+        let mut sum = 0.0;
+        for x in 0..self.order as usize {
+            sum += self.weights[x] * self.compute_exp(self.nodes[x]);
+        }
+        sum
     }
 }
 
@@ -28,8 +41,8 @@ mod tests {
 
     #[test]
     fn grid_initialisation_cheby() {
-        let grid = ChebyshevGauss::new(0.0, PI, 6);
-        println!("{:?}", grid);
+        let grid = ChebyshevGauss::new(-1.0, 1.0, 8);
+        println!("{:?}", grid.integrate());
     }
 }
 
