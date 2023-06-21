816f6631e7dd7909a316b139c8a785fa91aa50ae
2auk <ahmada1uk@googlemail.com>
2023-06-21 02:07:53 +0100

    offsets were computed incorrectly; fixed

--- grids/scratch/python_scripts/gen_lebedev.py
+++ grids/scratch/python_scripts/gen_lebedev.py
@@ -6,11 +6,20 @@ tables_path = Path('./tables').resolve()
 
 data_paths = sorted(list(tables_path.rglob('*.txt')))
 
-order_list = []
+offset_list = []
 const_list = []
 
 with open('lebedev_data.rs', 'w') as f:
-    f.write(f"use arrayvec::ArrayVec;\n")
+    f.write(f"use phf::phf_map;\n")
+    total = 0
+    for path in data_paths:
+        PHI, THETA, WEIGHTS = np.loadtxt(path, unpack=True)
+        ORDER = PHI.shape[0]
+        total += ORDER
+
+    offset_index = 0
+
+    f.write(f"pub const LD_DATA: [(f64, f64, f64); {total}] = [\n")
     for path in data_paths:
         PHI, THETA, WEIGHTS = np.loadtxt(path, unpack=True)
 
@@ -20,18 +29,19 @@ with open('lebedev_data.rs', 'w') as f:
 
         name = "LD" + order_name
 
-        order_list.append(ORDER)
+        offset_list.append((offset_index, offset_index + ORDER))
+        offset_index += ORDER
         const_list.append(name)
-
-        f.write(f"pub const {name}: ArrayVec<(f64, f64, f64), {ORDER}> = ArrayVec::from([\n")
+        
         for i in range(ORDER):
             phi = PHI[i]
             theta = THETA[i]
             weight = WEIGHTS[i]
             f.write(f"    ({theta}, {phi}, {weight}),\n")
-        f.write(f"]);\n\n")
 
-    f.write(f'static LDGRIDS: phf::Map<&\'static str, [(f64, f64, f64); N]> = phf_map! {{\n')
-    for order, arr_name in zip(order_list, const_list):
-        f.write(f'    "{order}" => {arr_name},\n')
+    f.write(f"];\n\n")
+
+    f.write(f'static LDGRIDS: phf::Map<&\'static str, (usize, usize)> = phf_map! {{\n')
+    for offset, arr_name in zip(offset_list, const_list):
+        f.write(f'    "{arr_name}" => {offset},\n')
     f.write(f'}};\n')
