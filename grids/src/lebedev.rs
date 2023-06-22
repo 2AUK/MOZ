d30343591fe05370622252318d9f5b625929832c
2auk <ahmada1uk@googlemail.com>
2023-06-22 11:35:06 +0100

    added github action workflow

--- grids/src/lebedev.rs
+++ grids/src/lebedev.rs
@@ -1,6 +1,6 @@
+use crate::lebedev_data::{get_lebedev_grid, LebedevGrid};
 use std::f64::consts::PI;
 use std::iter::zip;
-use crate::lebedev_data::{LebedevGrid, get_lebedev_grid};
 
 #[derive(Debug, PartialEq)]
 
@@ -14,7 +14,7 @@ impl LebedevLaikovGrid {
         let grid = get_lebedev_grid(grid);
         let mut weight = Vec::new();
         let mut coord = Vec::new();
-        for elem in grid{
+        for elem in grid {
             weight.push(elem.2);
             coord.push((elem.0, elem.1));
         }
@@ -23,7 +23,10 @@ impl LebedevLaikovGrid {
 
     pub fn integrate(&self, f: fn(&f64, &f64) -> f64) -> f64 {
         let grid_iter = zip(&self.weight, &self.coord);
-        4.0 * PI * grid_iter.map(|(weight, (theta, phi))| weight * f(theta, phi)).sum::<f64>()
+        4.0 * PI
+            * grid_iter
+                .map(|(weight, (theta, phi))| weight * f(theta, phi))
+                .sum::<f64>()
     }
 }
 
@@ -60,8 +63,15 @@ mod tests {
     #[test]
     fn grid_initialisation() {
         let grid = LebedevLaikovGrid::new(LebedevGrid::LD003);
-        let test_grid = LebedevLaikovGrid { 
-            weight: vec![0.166666666666667, 0.166666666666667, 0.166666666666667, 0.166666666666667, 0.166666666666667, 0.166666666666667],
+        let test_grid = LebedevLaikovGrid {
+            weight: vec![
+                0.166666666666667,
+                0.166666666666667,
+                0.166666666666667,
+                0.166666666666667,
+                0.166666666666667,
+                0.166666666666667,
+            ],
             coord: vec![
                 (90.0, 0.0),
                 (90.0, 180.0),
@@ -69,7 +79,7 @@ mod tests {
                 (90.0, -90.0),
                 (0.0, 90.0),
                 (180.0, 90.0),
-            ]
+            ],
         };
         assert_eq!(grid, test_grid);
     }
@@ -79,7 +89,7 @@ mod tests {
         // evaluates to -(2/3) * 2 * pi
         let grid = LebedevLaikovGrid::new(LebedevGrid::LD131);
         let integral = grid.integrate(cos_2theta);
-        assert_abs_diff_eq!((-2.0 / 3.0 * 2.0 * PI), integral, epsilon=PRECISION);
+        assert_abs_diff_eq!((-2.0 / 3.0 * 2.0 * PI), integral, epsilon = PRECISION);
     }
 
     #[test]
@@ -87,7 +97,7 @@ mod tests {
         // evaluates to 0
         let grid = LebedevLaikovGrid::new(LebedevGrid::LD131);
         let integral = grid.integrate(one_plus_three_cos_2theta);
-        assert_abs_diff_eq!(0.0, integral, epsilon=PRECISION);
+        assert_abs_diff_eq!(0.0, integral, epsilon = PRECISION);
     }
 
     #[test]
@@ -95,7 +105,7 @@ mod tests {
         // evaluates to 4.0 * pi / (2.0 * sqrt(pi))
         let grid = LebedevLaikovGrid::new(LebedevGrid::LD131);
         let integral = grid.integrate(y00);
-        assert_abs_diff_eq!((4.0 * PI / 2.0 / PI.sqrt()), integral, epsilon=PRECISION);
+        assert_abs_diff_eq!((4.0 * PI / 2.0 / PI.sqrt()), integral, epsilon = PRECISION);
     }
 
     #[test]
@@ -103,7 +113,7 @@ mod tests {
         // evaluates to 0
         let grid = LebedevLaikovGrid::new(LebedevGrid::LD131);
         let integral = grid.integrate(y01);
-        assert_abs_diff_eq!(0.0, integral, epsilon=PRECISION);
+        assert_abs_diff_eq!(0.0, integral, epsilon = PRECISION);
     }
 
     #[test]
@@ -111,6 +121,6 @@ mod tests {
         // evaluates to 0
         let grid = LebedevLaikovGrid::new(LebedevGrid::LD131);
         let integral = grid.integrate(y02);
-        assert_abs_diff_eq!(0.0, integral, epsilon=PRECISION);
+        assert_abs_diff_eq!(0.0, integral, epsilon = PRECISION);
     }
 }
