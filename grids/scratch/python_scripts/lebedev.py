9f3f2ea0983905aa73fd4c120e7c45615aeba5e4
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-12-05 14:34:48 +0000

    Plotting sph harm
    Looking correct, but sorta squashed?

--- grids/scratch/python_scripts/lebedev.py
+++ grids/scratch/python_scripts/lebedev.py
@@ -1,6 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as axes3d
 
-phi, theta, weights = np.loadtxt("lebedev_009.txt", unpack=True)
+phi, theta, weights = np.loadtxt("tables/lebedev_013.txt", unpack=True)
 
 order = phi.shape[0]
 
@@ -13,10 +15,10 @@ def zero(phi, theta):
 def Y00(phi, theta):
     return 1.0 / (2.0 * np.sqrt(np.pi))
 
-def Y01(phi, theta):
+def Y10(phi, theta):
     return 0.5 * np.sqrt(3.0 / np.pi) * np.cos(np.radians(theta))
 
-def Y02(phi, theta):
+def Y20(phi, theta):
     return 0.25 * np.sqrt(5.0 / np.pi) * (3.0 * np.cos(np.radians(theta)) * np.cos(np.radians(theta)) - 1.0)
 
 def integrate(f):
@@ -25,4 +27,19 @@ def integrate(f):
         sum += weights[i] * f(phi[i], theta[i])
     return 4.0 * np.pi * sum
 
-print(integrate(zero), integrate(cos2theta), integrate(Y00), integrate(Y01), integrate(Y02))
+print(integrate(zero), integrate(cos2theta), integrate(Y00), integrate(Y10), integrate(Y20))
+
+THETA, PHI = np.deg2rad(phi), np.deg2rad(theta)#np.meshgrid(np.deg2rad(theta), np.deg2rad(phi))
+R = np.abs(Y20(np.rad2deg(PHI), np.rad2deg(THETA)))
+X = R * np.sin(PHI) * np.cos(THETA)
+Y = R * np.sin(PHI) * np.sin(THETA)
+Z = R * np.cos(PHI)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+plot = ax.scatter(
+    X, Y, Z
+    )
+
+plt.show()
+
